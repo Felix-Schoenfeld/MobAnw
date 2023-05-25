92a9d181af37558352a930a050f927ae6245c5e7
Felix Schönfeld <felix.schoenfeld.00@gmail.com>
2023-05-25 10:19:33 +0200

    onCLickListener für MainActivity Buttons

--- app/src/main/java/de/fh_zwickau/pti/mobanw/ci_application/MainActivity.java
+++ app/src/main/java/de/fh_zwickau/pti/mobanw/ci_application/MainActivity.java
@@ -2,7 +2,11 @@ package de.fh_zwickau.pti.mobanw.ci_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // List CIs
+        Button buttonCIList = (Button) findViewById(R.id.buttonCIList);
+        buttonCIList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CIListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Fav List
+        Button buttonFavs = (Button) findViewById(R.id.buttonFavs);
+        buttonCIList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FavListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // CI Creation
+        Button buttonCreateCI = (Button) findViewById(R.id.buttonCreateCI);
+        buttonCIList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CICreationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
