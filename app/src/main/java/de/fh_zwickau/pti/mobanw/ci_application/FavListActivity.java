938a2d4acaa4e6f440efc8f67e2ccffe0ab67311
Felix Schönfeld <felix.schoenfeld.00@gmail.com>
2023-05-25 10:07:41 +0200

    Buttons und Activities erstellt

---
+++ app/src/main/java/de/fh_zwickau/pti/mobanw/ci_application/FavListActivity.java
@@ -0,0 +1,14 @@
+package de.fh_zwickau.pti.mobanw.ci_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fav_list);
+    }
+}
